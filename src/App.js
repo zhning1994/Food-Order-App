088b86732a3ba816b31c016b767421aa1a9909f2
Low Zhen Ning <lowzn94@gmail.com>
2022-03-10 22:12:11 +0800

    specified the color and display of header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-import Header from './components/Header';
+import Header from './components/Layout/Header';
 
 function App() {
-  return <Header></Header>;
+  return <Header />;
 }
 
 export default App;
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,6 +1,13 @@
-import './Header.css';
-function Header() {
-  return <div className='header'></div>;
-}
+import classes from './Header.module.css';
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>Welcome to Melhor Comida</h1>
+      </header>
+    </>
+  );
+};
 
 export default Header;
