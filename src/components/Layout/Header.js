c12765cb8a6b34f4262f4e3aa3de23fa83b4ecae
Low Zhen Ning <lowzn94@gmail.com>
2022-03-10 14:38:27 +0800

    Amend the file structure and add in header component to do testing

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,6 @@
+import './Header.css';
+function Header() {
+  return <div className='header'></div>;
+}
+
+export default Header;
---
+++ src/components/Services.js
@@ -0,0 +1,9 @@
+import './Services.css';
+
+function ServicesWeProvide() {
+  const services = 'Take Away';
+
+  return <div>{services}</div>;
+}
+
+export default ServicesWeProvide;
