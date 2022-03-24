1df0f74f6a2817b9c95a3260fcda807939662c2d
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-03-24 20:04:50 +0800

    create authentication and pages component

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,16 @@
+import classes from './Home.module.css';
+import Header from '../../components/Layout/Header';
+import Features from '../../components/Layout/Features';
+import Footer from '../../components/Layout/Footer';
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Features />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
