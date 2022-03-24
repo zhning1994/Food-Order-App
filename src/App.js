cf28c150322d63f848118c246f539f5b5aa1c45e
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-03-24 20:01:27 +0800

    insert the section into apps

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Layout/Header';
 import Features from './components/Layout/Features';
+import Menu from './components/Menu/Menu';
 
 function App() {
   return (
     <>
       <Header />
       <Features />
+      <Menu />
     </>
   );
 }
